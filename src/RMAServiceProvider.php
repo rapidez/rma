bd6656c38d3226ae6f1e508d6de1c8d3be75e960
Roy Duineveld <royduineveld@gmail.com>
2026-06-17 14:27:14 +0200

    Init

--- src/RMAServiceProvider.php
+++ src/RMAServiceProvider.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Rapidez\RMA;
 
 use Illuminate\Support\ServiceProvider;
 
-class SkeletonServiceProvider extends ServiceProvider
+class RMAServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/rapidez/:package_slug_without_prefix.php', 'rapidez.:package_slug_without_prefix');
+        $this->mergeConfigFrom(__DIR__.'/../config/rapidez/rma.php', 'rapidez.rma');
     }
 
     public function boot()
@@ -30,7 +30,7 @@ class SkeletonServiceProvider extends ServiceProvider
 
     public function bootViews() : self
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'rapidez-:package_slug_without_prefix');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'rapidez-rma');
 
         return $this;
     }
@@ -38,12 +38,12 @@ class SkeletonServiceProvider extends ServiceProvider
     public function bootPublishables() : self
     {
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/rapidez-:package_slug_without_prefix'),
-        ], 'rapidez-:package_slug_without_prefix-views');
+            __DIR__.'/../resources/views' => resource_path('views/vendor/rapidez-rma'),
+        ], 'rapidez-rma-views');
 
         $this->publishes([
-            __DIR__.'/../config/rapidez/:package_slug_without_prefix.php' => config_path('rapidez/:package_slug_without_prefix.php'),
-        ], 'rapidez-:package_slug_without_prefix-config');
+            __DIR__.'/../config/rapidez/rma.php' => config_path('rapidez/rma.php'),
+        ], 'rapidez-rma-config');
 
         return $this;
     }
